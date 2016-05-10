c4d07452a123a29445a166b0a00985e3e0c7513c
Gaurav Sood <gsood07@gmail.com>
2016-05-10 10:40:22 -0400

    changed encoding, but still no luck

--- data-raw/in/in.R
+++ data-raw/in/in.R
@@ -5,4 +5,5 @@ Indian Pol. Data
 # setwd("data-raw/in/")
 indian_pols <- read.csv("data/myNetas.csv")
 indian_pols <- indian_pols[, c("politician_name", "gender")]
+Encoding(indian_pols$politician_name) <- "utf8"
 use_data(indian_pols, compress="bzip2", overwrite=TRUE)
