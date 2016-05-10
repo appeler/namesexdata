4a4ce2bb22f761ff34484b294ed47ce2f02cfbdb
Gaurav Sood <gsood07@gmail.com>
2016-05-10 12:55:18 -0400

    it works

--- data-raw/in/in.R
+++ data-raw/in/in.R
@@ -5,5 +5,16 @@ Indian Pol. Data
 # setwd("data-raw/in/")
 indian_pols <- read.csv("data/myNetas.csv")
 indian_pols <- indian_pols[, c("politician_name", "gender")]
-Encoding(indian_pols$politician_name) <- "utf8"
+
+# Cleaning
+indian_pols$politician_name <- gsub(" \\(Winner\\)", "", indian_pols$politician_name)
+indian_pols$politician_name <- tolower(indian_pols$politician_name)
+
+# Just first names
+
+# Keep unique only
+indian_pols <- indian_pols[!duplicated(indian_pols$politician_name), ]
+
+# Fix encoding
+Encoding(indian_pols$politician_name) <- "latin1"
 use_data(indian_pols, compress="bzip2", overwrite=TRUE)
