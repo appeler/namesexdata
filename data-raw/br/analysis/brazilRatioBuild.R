e272a8bf9268f34dc660a179adfd5c952adf8188
Luke Sonnet <luke.sonnet@gmail.com>
2016-07-20 17:49:49 -0700

    Cleaned up brazilian dataset

--- data-raw/br/analysis/brazilRatioBuild.R
+++ data-raw/br/analysis/brazilRatioBuild.R
@@ -2,18 +2,23 @@
 # Purpose: Adds row of ratios, sorts by them and total count, and returns
 #          final dataset
 #
-# R v: 3.2.3
+# R v: 3.2.5
 
 dat <- read.csv("../data/brazilNamesSexCounts.csv", as.is=T)
+
+## Clean up some clear errors and convert to lower
+dat$firstName <- tolower(gsub("[\\,\\?\\'\\.]", "", dat$firstName))
+dat$firstName <- gsub("0", "o", dat$firstName)
+
+## combine some rows that now have the same name
+sum(duplicated(dat$firstName))
+dat <- aggregate(dat[c('total', 'female', 'male')], by=dat['firstName'], sum)
 
 ## Generate ratios
 dat$totalFound <- dat$female + dat$male
 
-dat$prFemale <- dat$female/dat$totalFound
+dat$pr_women <- dat$female/dat$totalFound
 
-out <- dat[order(dat$prFemale, dat$firstName, dat$totalFound),]
-
-## Clean up errant commas
-out$firstName <- gsub(",", "", out$firstName)
+out <- dat[order(dat$pr_women, dat$firstName, dat$totalFound),]
 
 write.csv(out, "../data/brazilNamesSexRatio.csv", row.names=F)
--- data-raw/br/br.R
+++ data-raw/br/br.R
@@ -3,6 +3,7 @@ Brazilian Pol. Data
 
 "
 
-brazilian_pols <- read.csv("data/brazilNamesSexRatio.csv")
+brazilian_pols <- read.csv("data/brazilNamesSexRatio.csv",
+                           stringsAsFactors = F)
 Encoding(brazilian_pols$firstName) <- "utf8"
-use_data(brazilian_pols, compress="xz", overwrite=TRUE)
+devtools::use_data(brazilian_pols, compress="xz", overwrite=TRUE)
