730a2aef459fa37d90e30a449a98dfbd419a51bc
Luke Sonnet <luke.sonnet@gmail.com>
2016-07-20 18:46:50 -0700

    Added rows to make matching without accents easier

--- data-raw/br/analysis/brazilRatioBuild.R
+++ data-raw/br/analysis/brazilRatioBuild.R
@@ -7,18 +7,25 @@
 dat <- read.csv("../data/brazilNamesSexCounts.csv", as.is=T)
 
 ## Clean up some clear errors and convert to lower
-dat$firstName <- tolower(gsub("[\\,\\?\\'\\.]", "", dat$firstName))
-dat$firstName <- gsub("0", "o", dat$firstName)
+dat$first_name <- tolower(gsub("[\\,\\?\\'\\.]", "", dat$firstName))
+dat$first_name <- gsub("0", "o", dat$first_name)
 
 ## combine some rows that now have the same name
-sum(duplicated(dat$firstName))
-dat <- aggregate(dat[c('total', 'female', 'male')], by=dat['firstName'], sum)
+sum(duplicated(dat$first_name))
+dat <- aggregate(dat[c('total', 'female', 'male')], by=dat['first_name'], sum)
 
 ## Generate ratios
-dat$totalFound <- dat$female + dat$male
+dat$total_found <- dat$female + dat$male
 
-dat$pr_women <- dat$female/dat$totalFound
+dat$pr_women <- dat$female/dat$total_found
 
-out <- dat[order(dat$pr_women, dat$firstName, dat$totalFound),]
+out <- dat[order(dat$pr_women, dat$first_name, dat$total_found),]
+
+## Create mean without accents
+out$first_name_noaccent <- as.factor(iconv(out$first_name, to = "UTF-8//TRANSLIT"))
+pr_women_noaccent <- by(out, out$first_name_noaccent, function(x) weighted.mean(x$pr_women, x$total_found))
+out$pr_women_noaccent <- pr_women_noaccent[out$first_name_noaccent]
+out$first_name_noaccent <- as.character(out$first_name_noaccent)
+out$pr_women_noaccent <- c(unname(out$pr_women_noaccent))
 
 write.csv(out, "../data/brazilNamesSexRatio.csv", row.names=F)
--- data-raw/br/br.R
+++ data-raw/br/br.R
@@ -5,5 +5,5 @@ Brazilian Pol. Data
 
 brazilian_pols <- read.csv("data/brazilNamesSexRatio.csv",
                            stringsAsFactors = F)
-Encoding(brazilian_pols$firstName) <- "utf8"
+Encoding(brazilian_pols$first_name) <- "utf8"
 devtools::use_data(brazilian_pols, compress="xz", overwrite=TRUE)
